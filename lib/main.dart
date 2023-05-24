f10ef37e52ff1a11d882625df2b53b027f8e3c59
Zane <zanejvv1997@gmail.com>
2023-05-24 20:21:36 +0200

    Finished tutorial 7 of the net ninja series.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text("Hello world"),
+    home: Home(),
   ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Tutorial"),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+
+      body: Center(
+        child: Text(
+            "Hello world",
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: "ReenieBeanie",
+            )
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        backgroundColor: Colors.red,
+        child: Text("Click"),
+      ),
+
+    );
+  }
 }
